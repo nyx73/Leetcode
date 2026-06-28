09b0d4b43ba55b43a0ca36092e2d65222c162019
Rhythm Chavda <152694036+nyx73@users.noreply.github.com>
2026-06-28 21:32:08 +0530

    Time: 263 ms (23.2%), Space: 31.8 MB (38.4%) - LeetHub

---
+++ 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.py
@@ -0,0 +1,18 @@
+class Solution:
+  def maximumLength(self, nums: list[int]) -> int:
+    maxNum = max(nums)
+    count = collections.Counter(nums)
+    ans = count[1] - (count[1] % 2 == 0) if 1 in count else 1
+
+    for num in nums:
+      if num == 1:
+        continue
+      length = 0
+      x = num
+      while x <= maxNum and x in count and count[x] >= 2:
+        length += 2
+        x *= x
+   
+      ans = max(ans, length + (1 if x in count else -1))
+
+    return ans
